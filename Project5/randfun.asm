610aca0d699f57bdd15291251a07ddfa28ad2f5b
Tristan Thompson <catsrul10@gmail.com>
2017-03-19 15:03:10 -0700

    Finished project 6!

--- Project5/randfun.asm
+++ Project5/randfun.asm
@@ -11,14 +11,19 @@ REQUEST_MAX = 200
 RANDOM_MIN = 100
 RANDOM_MAX = 999
 
+NUMS_PER_LINE = 10
+
 .data
 introMessage BYTE "My name is Tristan Thompson and this is Random Operations!",0
 requestMessage BYTE "Give me a number of random numbers between 10 and 200: ",0
 errorMessage BYTE "No, idiot!",0
-arrayTitleMessage BYTE "Here are ya random nums boi:",0
+arrayUnsortedTitleMessage BYTE "Unsorted numbers:",0
+arraySortedTitleMessage BYTE "Sorted numbers:",0
+arrayMedianMessage BYTE "Median: ",0
 request DWORD ?
-randomNums DWORD REQUEST_MAX-REQUEST_MIN DUP(?)
+randomNums DWORD REQUEST_MAX + 1 DUP(?)
 
+spaces BYTE "   ",0
 
 .code
 main PROC
@@ -33,15 +38,29 @@ main PROC
 
 	push OFFSET randomNums
 	push request
-	push OFFSET arrayTitleMessage
+	push OFFSET arrayUnsortedTitleMessage
 	call displayList
 
+	push OFFSET randomNums
+	push request
+	call sortList
+
+	push OFFSET randomNums
+	push request
+	push OFFSET arraySortedTitleMessage
+	call displayList
+
+	push OFFSET randomNums
+	push request
+	call displayMedian
+
+	call ReadInt
 ; (insert executable instructions here)
 exit
 main ENDP
 
 ;Write introMessage
-introduction PROC
+introduction PROC USES edx
 	mov edx, OFFSET introMessage
 	call WriteString
 	call CrLf
@@ -54,53 +73,68 @@ introduction ENDP
 getData PROC
 	push ebp
 	mov ebp, esp
-	requestAddr EQU [ebp + 8]
+	requestAddr EQU [ebp + TYPE DWORD * 2]
 	
 	requestLoop:
 		mov edx, OFFSET requestMessage
 		call WriteString
 		call ReadInt
+
+		;Check for errors
 		cmp eax, REQUEST_MIN
 		jl requestError
 		cmp eax, REQUEST_MAX
 		jg requestError
+
 		jmp requestDone
+		
 		requestError:
 			mov edx, OFFSET errorMessage
 			call WriteString
 			call CrLf
 			jmp requestLoop
+
 	requestDone:
+
+	;Store request into given address
 	mov esi, requestAddr
 	mov [esi], eax
+	
 	pop ebp
-	ret
+	ret TYPE DWORD
 getData ENDP
 
 ;Fill a given array with random numbers
 ;Paramters:
 ;Request: the amount of numbers to generate
 ;Array: A reference to the array to fill
-fillArray PROC
+fillArray PROC 
 	push ebp 
 	mov ebp, esp
 	sub esp , TYPE DWORD
+
 	fills EQU [ebp - 4]
 	paramRequest EQU [ebp + 12]
 	paramArray EQU [ebp + 8]
 
 	mov esi, 0
+
 	mov eax, 0
 	mov fills, eax	
+
 	mov ebx, paramArray
 
+	call Randomize
+
 	fillLoop:
+		;100-999 = 0-899 + 100
 		mov eax, RANDOM_MAX - RANDOM_MIN
 		call RandomRange
 		add eax, RANDOM_MIN
 		
 		mov [esi+ebx], eax
 		
+		;Increment eax and fills
 		add esi, TYPE DWORD
 		mov eax, fills
 		add eax, 1
@@ -115,16 +149,167 @@ fillArray PROC
 	mov esp, ebp
 	pop ebp
 	
-	ret 
+	ret TYPE DWORD * 2
 fillArray ENDP
 
+;Sort a list
+;Parameters:
+;Array: a reference to the array to sort
+;Request: the size of the array
 sortList PROC
+	push ebp
+	mov ebp, esp
+	sub esp, 4
+	swapped EQU [ebp - 4]
+	sortArray EQU [ebp + 12]
+	sortRequest EQU [ebp + 8]
+
+	;Make sortRequest in terms of bytes for ease of iteration
+	mov eax, sortRequest
+	mov ebx, TYPE DWORD
+	mul ebx
+	mov sortRequest, eax
+
+	mov eax, 0
+	mov [swapped], eax
+
+	mov esi, sortArray
+	
+	sortLoop:
+		mov eax, TYPE DWORD ;1st index
+		mov ebx, 0 ;0th index
+		mov edx, 0
+		mov [swapped], edx
+		sortIteration:
+			mov edx, [esi + eax]	
+			cmp edx, [esi + ebx]
+			jle iterate
+
+			mov edx, 1
+			mov [swapped], edx
+
+			;Store relevant registers
+			push eax
+			push ebx
+
+			;Push the position of array[i] and array[i-1]
+			mov edx, esi
+			add edx, eax
+			push edx
+
+			mov edx, esi
+			add edx, ebx
+			push edx
+
+			call exchangeElements
+
+			;Restore relevant registers
+			pop ebx
+			pop eax 
+
+			iterate:
+				add eax, TYPE DWORD
+				add ebx, TYPE DWORD
+				cmp eax, sortRequest
+				jg doneIteration
+				jmp sortIteration
+				doneIteration:
+					;If there were no swaps, then the list is properly sorted.
+					mov edx, [swapped]
+					cmp edx, 0
+					jg sortLoop
+	mov esp, ebp
+	pop ebp
+	ret TYPE DWORD * 2
 sortList ENDP
 
-exchangeElements PROC
+;Exchange two elements in an array
+;Parameters:
+;array[i]: A reference to the ith element to swap
+;array[j]: A reference to the jth element to swap
+exchangeElements PROC 
+	push ebp
+	mov ebp, esp
+	arrayI EQU [ebp + 12]
+	arrayJ EQU [ebp + 8]
+	
+	;save arrayJ
+	mov eax, arrayJ
+	mov eax, [eax]
+	push eax
+	
+	;Write arrayI to arrayJ
+	mov eax, arrayJ
+
+	;Get the value of arrayI
+	mov ebx, arrayI
+	mov ebx, [ebx]
+	
+	mov [eax], ebx
+
+	;Write araryJ to arrayI
+	
+	;Get the value of arrayJ
+	pop ebx	
+	mov eax, arrayI
+	mov [eax], ebx
+
+	pop ebp
+	ret TYPE DWORD * 2
 exchangeElements ENDP
 
+;Calculate and display the median of a list.
+;Parameters:
+;Array: the array to calculate on
+;Request: the size of the array
 displayMedian PROC
+	push ebp
+	mov ebp, esp
+	medianArray EQU [ebp + 12]
+	medianRequest EQU [ebp + 8]
+	
+	mov esi, medianArray
+
+	mov edx, OFFSET arrayMedianMessage
+	call WriteString
+
+
+	;Calculate midpoint in bytes
+	cdq
+	mov eax, medianRequest
+	mov ebx, 2
+	div ebx
+	
+	;eax is off by one due to the way indexes are counted.
+	dec eax 
+
+	mov ebx, TYPE DWORD
+	mul ebx
+	
+	cmp edx, 0
+	jg displayOdd
+
+	;if the request is even, we need to take the average of the two values around the midpoint.
+	mov ebx, eax
+	add ebx, TYPE DWORD
+	
+	mov eax, [esi + eax]
+	mov ebx, [esi + ebx]
+	add eax, ebx
+	mov ebx, 2
+	div ebx
+	jmp done
+
+	;if the request is odd, we can directly display the midpoint
+	displayOdd:
+		add eax, TYPE DWORD
+		mov eax, [esi + eax]
+		
+	done:
+	call WriteDec
+
+	pop ebx
+	ret TYPE DWORD * 2
 displayMedian ENDP
 
 ;Display a list
@@ -132,13 +317,21 @@ displayMedian ENDP
 ;Array: the array to display
 ;Request: The amount of elements to display
 ;Title: A string reference to a message to display
-displayList PROC
-	LOCAL displays:DWORD
- 	paramArrayDisplay EQU [ebx + 16]
-	paramDisplayAmount EQU [ebx + 12]
-	paramArrayTitle EQU [ebx + 8]
+displayList PROC 
+	push ebp 
+	mov ebp, esp
+	sub esp , TYPE DWORD * 2
 
-	mov displays, 0
+	displays EQU [ebp - TYPE DWORD]
+	lineDisplays EQU [ebp - TYPE DWORD * 2]
+
+ 	paramArrayDisplay EQU [ebp + 16]
+	paramDisplayAmount EQU [ebp + 12]
+	paramArrayTitle EQU [ebp + 8]
+
+	mov eax, 0
+	mov displays, eax
+	mov lineDisplays, eax
 
 	mov edx, paramArrayTitle
 	call WriteString
@@ -154,24 +347,41 @@ displayList PROC
 		;Write current element
 		mov eax, [esi + ebx]
 		call WriteDec
-		xor eax, eax
-		mov al, ','
-		call WriteChar
-		mov al, ' ' 
-		call WriteChar		
+
+		mov edx, OFFSET spaces
+		call WriteString
 		
 		;Increment index and count
-		add displays, 1
-		add ebx, 4
 
 		mov eax, displays
-		cmp eax, ecx
-		je displayLoopDone
-		
-	displayLoopDone:
+		add eax, 1
+		mov displays, eax
 
-	leave
-	ret
+		add ebx, 4
+
+		;Increment line displays and check if at limit
+		mov eax, lineDisplays
+		add eax, 1
+		mov lineDisplays, eax
+
+		cmp eax, NUMS_PER_LINE
+		jl checkDone
+		
+		call CrLf
+		mov eax, 0
+		mov lineDisplays, eax
+
+		checkDone:
+			mov eax, displays
+			cmp eax, ecx
+			je displayLoopDone
+			jmp displayLoop
+	displayLoopDone:
+	call CrLf
+
+	mov esp, ebp
+	pop ebp
+	ret TYPE DWORD * 3
 displayList ENDP
 
 
--- Project6/composite.asm
+++ Project6/composite.asm
@@ -1,27 +1,444 @@
-TITLE Program Template (Template.asm)
-; Program Description:
-; Author:
-; Creation Date:
+TITLE Composite (composite.asm)
+; Program Description: Test the user on combinations
+; Author: Tristan Thompson
+; Creation Date: 3/18/17
 ; Revisions:
-; Date:
+; Date: 3/18/17
+
+MAX_N = 12
+MIN_N = 3
+RANGE_N = MAX_N - MIN_N
+
+m_putStr MACRO address
+	mov edx, address
+	call WriteString
+ENDM
+
+m_putStrLn MACRO address
+	mov edx, address
+	call WriteString
+	call CrLf
+ENDM
+
+m_randomRange MACRO min, max
+	mov eax, max
+	call RandomRange
+	add eax, min
+ENDM
+
 INCLUDE Irvine32.inc
 .data
-; (insert variables here)
+	invalidNumError BYTE "This isn't a number: ",0
+	introMessage BYTE "My name is Tristan and this is Composite Test!",0
+	problemMessageHead BYTE "What is ",0
+	problemMessageMid BYTE "C",0
+	problemMessageEnd BYTE "?",0
+	
+	answerMessage BYTE "You wrote: ",0
+	resultMessage BYTE "The answer was: ",0
+
+	incorrectMessage BYTE "Wrong, idiot!",0
+	correctMessage BYTE "You're right!",0
+
+	continueMessage BYTE "Do another? (y to continue, other to quit): ",0
+	continueAnswer BYTE 10 DUP(?)
+
+	num DWORD ?
+	combinationResult DWORD ?
+
+
+	n DWORD ?
+	r DWORD ?
+
 .code
 main PROC
-; (insert executable instructions here)
-exit
+
+	call introduction
+
+main_loop:
+
+	push OFFSET r
+	push OFFSET n
+	call showProblem
+
+	push OFFSET num
+	call getData
+
+	push num
+	push r
+	push n
+	push OFFSET combinationResult
+	call showResult
+	
+	m_putStr OFFSET continueMessage
+
+	mov edx, OFFSET continueAnswer
+	mov ecx, SIZEOF continueAnswer
+	call ReadString
+	
+	;If the answer has a lengh greater than 1 then it is invalid.
+	mov ecx, 1
+	cmp eax, ecx
+	jg main_done
+	
+	;Check if user gave a y, do another problem if so.
+	mov edx, OFFSET continueAnswer
+	mov al, 'y'
+	mov dl, BYTE PTR [edx]
+	cmp dl, al
+	je main_loop
+
+main_done:
+	exit
+
 main ENDP
-readNum PROC
+
+introduction PROC
+	;TODO: turn this into a macro
+	m_putStrLn OFFSET introMessage
+	ret
+introduction ENDP
+
+;Generate random n and r and display the problem.
+;Parameters:
+;r: address of r
+;n: address of n
+showProblem PROC
 	push ebp
 	mov ebp, esp
-	string TEXTEQU [ebp-8]
+	
+	;Save used registers
+	push eax
+	push ebx
+	push edx
+
+	showProblem_n EQU [ebp + TYPE DWORD + TYPE DWORD]
+	showProblem_r EQU [ebp + TYPE DWORD + TYPE DWORD * 2]
+
+	call Randomize
+	
+	;Calculate n
+	m_randomRange MIN_N, RANGE_N
+ 	
+	mov ebx, [showProblem_n]
+	mov [ebx], eax
+
+	;Calculate r
+	m_randomRange 1, eax
+
+
+	mov ebx, showProblem_r
+	mov [ebx], eax
+
+	;Display problem
+	m_putStr OFFSET problemMessageHead
+
+	mov eax, showProblem_n
+	mov eax, [eax]
+	call WriteDec
+
+	m_putStr OFFSET problemMessageMid
+
+	mov eax, showProblem_r
+	mov eax, [eax]
+	call WriteDec
+
+	m_putStrLn OFFSET problemMessageEnd
+
+
+	;Restore used registers
+	pop edx
+	pop ebx
+	pop eax
+	
+	pop ebp 
+
+	ret 8
+showProblem ENDP
+
+;Get user input
+;Parameters:
+;num: address of the value to store the input
+getData PROC
+	push ebp
+	mov ebp, esp
+	
+	sub esp, 10 * TYPE BYTE ;Max dword = ~10^9 + null char
+
+	;Save used registers
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+
+	getData_num EQU [ebp + TYPE DWORD + TYPE DWORD]
+	
+	getData_start:
 	
 	;Get the string
-	mov edx, string
+	mov edx, ebp
+	sub edx, 4
+	mov ecx, 10
 	call ReadString 
+	
+	;Initialize
+	mov esi, edx
+	xor eax, eax
+	cdq
+	cld
+	mov ecx, getData_num
+	mov ebx, 0
+	mov [ecx], ebx
+
+	getData_numLoop:
+
+		;Multiply num by 10
+		mov ecx, getData_num
+		mov ebx, 10
+		mov eax, [ecx]
+		mul ebx
+		mov [ecx], eax
+		mov eax, 0
+		;Get the next byte
+		lodsb
+		
+		;Stop at the null bit
+		cmp al, 0
+		je getData_done
+		
+		;check for invalid characters
+		cmp al, '0'
+		jl getData_error
+		cmp al, '9'
+		jg getData_error
+
+		;Convert char to int
+		sub al, '0'
+		
+		mov ecx, getData_num
+		
+		;Accumulate eax onto num
+		add [ecx], eax
+		jmp getData_numLoop
+
+	getData_error:
+		m_putStr OFFSET invalidNumError
+		call WriteChar
+		call CrLf
+		jmp getData_start
+		
+	getData_done:
+	
+	;Roll back one multiply
+	mov ebx, 10
+	mov ecx, [getData_num]
+	mov eax, [ecx]
+	div ebx
+
+	;Store at resut
+	mov [ecx], eax
+
+	;Restore registers
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	
+	mov esp, ebp
+	pop ebp
+
+	ret 4
+
+getData ENDP
+
+;Calculate nCr
+;Parameters:
+;r: the value of r
+;n: the value of n
+;result: the address of where to store the result
+combination PROC
+	push ebp
+	mov ebp, esp
+	
+	;Store used registers
+	push eax
+	push ebx
+
+	;We will have 3 local variables of size DWORD
+	sub esp, TYPE DWORD * 3
+	
+	combination_r EQU [ebp + TYPE DWORD + TYPE DWORD * 3]
+	combination_n EQU [ebp + TYPE DWORD + TYPE DWORD * 2]
+	combination_result EQU [ebp + TYPE DWORD + TYPE DWORD]
+	
+	combination_n_factorial EQU [ebp - TYPE DWORD]
+	combination_r_factorial EQU [ebp - TYPE DWORD * 2]
+	combination_n_minus_r_factorial EQU [ebp - TYPE DWORD * 3 ]
+
+	;calculate n!
+	push combination_n
+	lea eax, combination_n_factorial
+	push eax
+	call factorial
+
+	;calculate r!
+	push combination_r
+	lea eax, combination_r_factorial
+	push eax
+	call factorial
+
+	;Calculate (n-r)!
+	mov eax, combination_n
+	sub eax, combination_r
+	push eax
+	
+	lea eax, combination_n_minus_r_factorial
+	push eax
+	call factorial
+
+	;calculate r!(n-r)!
+	mov eax, combination_r_factorial
+	mov ebx, combination_n_minus_r_factorial
+	mul ebx
+
+	;calculate n!/(r!(n-r)!)
+	cdq
+	mov ebx, eax
+	mov eax, combination_n_factorial
+	div ebx
+
+	;Store at result
+	mov ebx, combination_result
+	mov [ebx], eax
+
+	;Restore used registers
+	pop ebx
+	pop eax
+
+	mov esp, ebp
+	pop ebp
+	ret 12
+	
+
+combination ENDP
+
+;Calculate n!
+;Parameters:
+;n: the value of factorial to calculate
+;result: the location of the place to store the result of the factorial
+factorial PROC
+	push ebp
+	mov ebp, esp
+
+	;Store used registers
+	push eax
+	push ebx
+
+	factorial_result EQU [ebp + TYPE DWORD + TYPE DWORD]
+	factorial_n EQU [ebp + TYPE DWORD + TYPE DWORD * 2]
+
+	mov eax, factorial_n
+	cmp eax, 0
+	jg factorial_recurse
+
+	;Base case
+	mov eax, 1
+	mov ebx, factorial_result
+	mov [ebx], eax ; 0! = 1
+	
+	jmp factorial_done
+
+	factorial_recurse:
+	
+	;Recursive step
+	dec eax
+	push eax
+	push factorial_result
+	call factorial
+
+	;After final recursion
+	mov ebx, factorial_result
+	mov ebx, [ebx]
+	
+	mov eax, factorial_n
+
+	mul ebx
+	mov ebx, factorial_result
+	mov [ebx], eax
+
+	factorial_done:
+		;Restore used registers
+		pop ebx
+		pop eax
+
+		pop ebp
+		ret 8
+factorial ENDP
+
+;Display the given answer, the calculation, and wether or not the user is correct.
+;Parameters
+;answer: the value of user given answer
+;r: the value of r
+;n: the value of n
+;result: the address of where to store the calculated combination
+showResult PROC
+	push ebp
+	mov ebp, esp
+	
+	;Store used registers
+	push eax
+
+	showResult_answer EQU [ebp + TYPE DWORD + TYPE DWORD * 4]
+	showResult_r EQU [ebp + TYPE DWORD + TYPE DWORD * 3]
+
+	showResult_n EQU [ebp + TYPE DWORD + TYPE DWORD * 2]
+	showResult_result EQU [ebp + TYPE DWORD + TYPE DWORD] 
+
+	;Perform the combination
+	push showResult_r
+	push showResult_n
+	push showResult_result
+	call combination
+
+	;Write the user's answer
+	mov edx, OFFSET answerMessage
+	call WriteString
+	
+	mov eax, showResult_answer
+	call WriteDec
+	call CrLf
+
+	;Write the combination result
+	mov edx, OFFSET resultMessage
+	call WriteString
+
+	mov eax, showResult_result
+	mov eax, [eax]
+	call WriteDec
+	call CrLf
+
+	;Show a message depending on the correctness of the user's answer
+	cmp eax, showResult_answer
+	je showResult_correct
+	
+	m_putStrLn OFFSET incorrectMessage
+	jmp showResult_done
+
+	showResult_correct:
+	m_putStrLn OFFSET correctMessage
+
+	showResult_done:
+
+	;Restore used registers
+	pop eax
+
+	pop ebp
+	ret 16
 
 
-readNum ENDP
-; (insert additional procedures here)
+showResult ENDP
+
+
 END main
